ac0a0978221d30a0fd5de328561e6832b8799514
abderrahmen <abderrahmanebouhlel@gmail.com>
2025-08-08 15:47:42 +0100

    routes controllers

---
+++ app/Http/Controllers/Api/V1/Admin/TeachersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TeachersController extends Controller
+{
+    public function index(Request $request){
+        return [] ;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
+// localhost//api/v1/auth
+// localhost//api/v1/admin
 
 Route::prefix('v1')->group(function(){
-        // Route::prefix('auth')->group(function(){
-        //     Route::post('login', '');
-        //     Route::middleware('auth.token')->post('logout', '');
-        // });
+        Route::prefix('auth')->group(function(){
+                Route::post('login', fn() => 'temp');
+                Route::middleware('auth.token')->post('logout', fn() => 'temp');
+        });
     
         // Route::prefix('admin')
         //     ->middleware(['auth.token', 'role:admin'])
         //     ->group(function () {
-        //         //Route::get('teachers', []);
+
+        //         Route::get('teachers', )
         //     });
     
     
